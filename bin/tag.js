8e77dd651805fcf9b7838b737030049c90e57cc3
Jake Scott <jscott@addthis.com>
2015-03-16 22:07:35 -0400

    tag versions on publish

---
+++ bin/tag.js
@@ -0,0 +1,5 @@
+var fs = require('fs');
+var pj = JSON.parse(fs.readFileSync('./package.json'));
+var version = pj.version;
+var exec = require('child_process').exec;
+exec('git tag -a v' + version + ' -m "Automaticly tagged for npm publish"');
