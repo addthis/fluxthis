ddb0f1970e69b2bf97ca52fff18489ef4b06bd7b
Alistair Brown <github@alistairjcbrown.com>
2017-12-01 13:58:12 +0000

    Update tests and runner (#162)

--- test/src/store-tests.js
+++ test/src/store-tests.js
@@ -13,32 +13,32 @@ describe('Store', function () {
 	});
 
 	it('should have addChangeListener', function () {
-		new Store(config).addChangeListener.should.be.a.func;
+		new Store(config).addChangeListener.should.be.a.Function;
 	});
 
 	it('should have deprecated addChangeListener', function () {
-		new Store(config).__addChangeListener.should.be.a.func;
+		new Store(config).__addChangeListener.should.be.a.Function;
 	});
 
 	it('should have removeChangeListener', function () {
-		new Store(config).removeChangeListener.should.be.a.func;
+		new Store(config).removeChangeListener.should.be.a.Function;
 	});
 
 	it('should have deprecated removeChangeListener', function () {
-		new Store(config).__removeChangeListener.should.be.a.func;
+		new Store(config).__removeChangeListener.should.be.a.Function;
 	});
 
 	it('should have waitFor', function () {
-		new Store(config).waitFor.should.be.a.func;
+		new Store(config).waitFor.should.be.a.Function;
 	});
 
 	it('should have a mixin object', function () {
 		var store = new Store(config);
 		var mixin = store.mixin;
 
-		mixin.componentDidMount.should.be.a.func;
-		mixin.componentWillUnmount.should.be.a.func;
-		mixin.componentWillUpdate.should.be.a.func;
-		mixin.getInitialState.should.be.a.func;
+		mixin.componentDidMount.should.be.a.Function;
+		mixin.componentWillUnmount.should.be.a.Function;
+		mixin.componentWillUpdate.should.be.a.Function;
+		mixin.getInitialState.should.be.a.Function;
 	});
 });
